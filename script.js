418a7f51dc17b282adf427f657d20494e70f7929
tsiryJL <tsiry.razanakoto@gmail.com>
2020-06-19 20:13:45 +0200

    NavBar

---
+++ script.js
@@ -0,0 +1,29 @@
+function myFunction(x) {
+    if (x.matches) { // si responsive
+     MeRemove();
+    } else {
+      MeAdd();
+    }
+  }
+  var element = document.getElementById("myDIV");
+  var Obj = document.getElementById("myToggle");
+  var x = window.matchMedia("(max-width: 991px)")
+  myFunction(x) // Call listener function at run time
+  x.addListener(myFunction) // Attach listener function on state changes
+
+
+  function MeRemove() {
+    element.classList.remove("navbar-expand-lg");
+    element.classList.remove("navbar-light");
+    element.classList.remove("float-right");
+    element.classList.remove("navbar");
+    Obj.classList.remove("navbar-toggler");
+  }
+
+  function MeAdd() {
+    element.classList.add("navbar-expand-lg");
+    element.classList.add("navbar-light");
+    element.classList.add("float-right");
+    element.classList.add("navbar");
+    Obj.classList.add("navbar-toggler");
+  }
